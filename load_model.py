41f1a11a8b0368a26d7ab706cb4d849ac6300205
Giorgos Livanos <livanos.g@gmail.com>
2021-07-06 20:07:27 +0300

    setup training

--- load_model.py
+++ load_model.py
@@ -1,13 +1,14 @@
 import cv2
 import numpy as np
 import tensorflow as tf
-from losses import dice_coef_loss, log_dice_loss
+from losses import log_dice_loss
 from metrics import f1
 
 model = tf.keras.models.load_model('model_060721183636', custom_objects={'log_dice_loss': log_dice_loss,
-                                                             'f1': f1})
+                                                                         'f1': f1})
 
-image = cv2.resize(cv2.imread("/home/livanosg/projects/mel-seg/data/ph2/data/IMD015/IMD015_Dermoscopic_Image/IMD015.bmp"), (224, 224))
+image = cv2.resize(
+    cv2.imread("/home/livanosg/projects/mel-seg/data/ph2/data/IMD015/IMD015_Dermoscopic_Image/IMD015.bmp"), (224, 224))
 output = model.predict(tf.expand_dims(image, axis=0))
 
 output = output[0, :, :, 0]
